c434972be8374b4fc0029835608c09aaeaa5dd2a
Marco Vuillermoz <marcovuillermoz@A21408.local>
2018-11-05 11:59:04 +0000

    small changes

--- src/js/App.jsx
+++ src/js/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
-import HeaderHero from './components/structural/Menu';
+import MenuNavigation from './components/structural/MenuNavigation';
 import MainContainer from './components/structural/MainContainer';
 
 const App = () => (
